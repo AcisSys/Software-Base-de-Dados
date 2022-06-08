e485a1f7142ed1f0902889f9d61ab69cb4925580
Alex Dias <alexfdias1801@gmail.com>
2022-06-08 10:32:06 +0100

    Delete Documentation

--- Tasks.cs
+++ Tasks.cs
@@ -25,7 +25,6 @@ namespace Software_Base_de_Dados
 
         private void Tasks_Load(object sender, EventArgs e)
         {
-            // Verifica a conexao
             if (connection.State == ConnectionState.Closed)
             {
                 try
@@ -41,7 +40,6 @@ namespace Software_Base_de_Dados
             }
             if (Tipo == "Add")
             {
-                // ID é automatico
                 string comand = "SELECT MAX (ID) FROM tab_tasks";
                 OleDbCommand oleDbCommand = new OleDbCommand(comand, connection);
                 int maxid = (int)oleDbCommand.ExecuteScalar();
@@ -61,7 +59,6 @@ namespace Software_Base_de_Dados
                     checkBox1.Checked = false;
                 }
             }
-            // Dados para ComboBox1
             querry = "SELECT * FROM tab_places";
             adapter = new OleDbDataAdapter(querry, connection);
             adapter.Fill(dset, "idtask");
@@ -98,7 +95,6 @@ namespace Software_Base_de_Dados
                 {
                     active = "Não";
                 }
-                // querry para adicionar dados a tabela
                 if (Tipo == "Add")
                 {
                     querry = "INSERT INTO tab_tasks (ID, Descricao, IDPlace, Active, RefTag)" +
@@ -110,20 +106,16 @@ namespace Software_Base_de_Dados
                     oleDbCommand.Parameters.Add("@Active", OleDbType.LongVarChar).Value = active;
                     oleDbCommand.Parameters.Add("@RefTag", OleDbType.Integer).Value = sfComboBox2.Text;
                 }
-                // querry para modificar dados
                 else
                 {
                     querry = "UPDATE tab_tasks  Descricao = @Descricao, IDPlace = @IDPlace, Active = @Active, RefTag = @RefTag" +
                        " WHERE ID = " + maskedTextBox1.Text;
                     oleDbCommand = new OleDbCommand(querry, connection);
-                    // Recebe os dados
                     oleDbCommand.Parameters.Add("@Descricao", OleDbType.Integer).Value = int.Parse(maskedTextBox2.Text);
                     oleDbCommand.Parameters.Add("@IDPlace", OleDbType.Integer).Value = int.Parse(sfComboBox1.Text);
                     oleDbCommand.Parameters.Add("@Active", OleDbType.LongVarChar).Value = active;
                     oleDbCommand.Parameters.Add("@RefTag", OleDbType.Integer).Value = int.Parse(sfComboBox2.Text);
                 }
-                // Executa o Comando
-                // Envia mensagem de erro / sucesso
                 try
                 {
                     oleDbCommand.ExecuteNonQuery();
@@ -141,7 +133,6 @@ namespace Software_Base_de_Dados
                 MessageBox.Show("Dados modificados com sucesso", "",
                     MessageBoxButtons.OK
                     , MessageBoxIcon.Information);
-                // limpa todos os campos e fecha  a janela de introdução de dados
                 maskedTextBox1.Text = "";
                 sfComboBox1.Text = "";
                 sfComboBox2.Text = "";
