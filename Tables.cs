1c92b5b3e2860415dd092e2f0a6acfe30c786933
Alex Dias <alexfdias1801@gmail.com>
2022-06-15 17:33:22 +0100

    -

--- Tables.cs
+++ Tables.cs
@@ -239,7 +239,6 @@ namespace Software_Base_de_Dados
             {
                 try
                 {
-                    oleDbCommand.ExecuteNonQuery();
                     int rowsChanged = oleDbCommand.ExecuteNonQuery();
                     // Says that data was deleted
                     MessageBox.Show("Dados apagados com sucesso", "",
