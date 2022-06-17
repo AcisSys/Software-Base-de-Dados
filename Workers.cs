fd52ca67f1c6b1647908df53a01841001b1e7bf0
Alex Dias <alexfdias1801@gmail.com>
2022-06-17 10:48:58 +0100

    -

--- Workers.cs
+++ Workers.cs
@@ -114,7 +114,6 @@ namespace Software_Base_de_Dados
         {
             sfToolTip1.Hide();
         }
-
         private void Workers_FormClosed(object sender, FormClosedEventArgs e)
         {
             sfComboBox1.Text = "";
