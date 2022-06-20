e4ee078f339e9de2734aa2d44713b0c4af41e5b1
Alex Dias <alexfdias1801@gmail.com>
2022-06-20 15:50:44 +0100

    .

--- Agend.cs
+++ Agend.cs
@@ -16,7 +16,6 @@ namespace Software_Base_de_Dados
         public string Tipo { get; set; }
         public int Id { get; set; }
         public string Idequipa { get; set; }
-
         public string Idtask { get; set; }
         private void Agend_Load(object sender, EventArgs e)
         {
